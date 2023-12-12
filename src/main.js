26d8c150d6e65c900945785972d3ae217beffe5f
taniot <gaetano.frascolla@gmail.com>
2023-12-12 12:15:49 +0100

    script: font montserrat

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 //file di partenza stili progetto
 import './assets/scss/style.scss';
+//font montserrat
+import '@fontsource/montserrat';
 //file di partenza Vue
 import App from './App.vue';
 
