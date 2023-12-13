4ac5968831a5dc32ffab0f492abfb587fe970f46
taniot <gaetano.frascolla@gmail.com>
2023-12-13 13:39:20 +0100

    feat: footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+import CharacterFound from './CharacterFound.vue';
+export default {
+  name: 'Footer',
+  components: {
+    CharacterFound,
+  },
+};
+</script>
+
+<template>
+  <footer class="d-flex justify-content-center my-5">
+    <CharacterFound />
+  </footer>
+</template>
