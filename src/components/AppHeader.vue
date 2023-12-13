fee2ef80974de0ec5e67cbfed37653da247f5a68
taniot <gaetano.frascolla@gmail.com>
2023-12-13 13:39:37 +0100

    refactor: header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,7 +12,6 @@ export default {
   <header>
     <div class="container my-5">
       <h1 class="text-center">Rick and Morty App</h1>
-      <CharacterFound />
     </div>
   </header>
 </template>
